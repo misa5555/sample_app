36b19048fdcc965e24673c1cc5157e330d9e26f2
michalle <misa.gk@icloud.com>
2014-06-26 15:49:33 -0700

    all changes

--- Gemfile
+++ Gemfile
@@ -6,12 +6,12 @@ gem 'rails', '4.0.5'
 
 group :development, :test do
   gem 'sqlite3', '1.3.8'
-  gem 'rspec-rails', '2.13.1'
+  gem 'rspec-rails'
 end
 
 group :test do
   gem 'selenium-webdriver', '2.35.1'
-  gem 'capybara', '2.1.0'
+  gem 'capybara','1.1.4'
 end
 
 gem 'sass-rails', '4.0.2'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,13 +1,10 @@
 require 'spec_helper'
 
-describe "Static pages" do
-
-  describe "Home page" do
-
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+RSpec.describe "StaticPages", :type => :request do
+  describe "GET /static_pages" do
+    it "works! (now write some real specs)" do
+      get static_pages_index_path
+      expect(response.status).to be(200)
     end
   end
 end
-
