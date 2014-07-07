755a43d23c2a1743e49436c518a0c5a89092b9e3
michalle <misa.gk@icloud.com>
2014-07-06 17:56:11 -0700

    inplemented passing the value of in_reply_to to database

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
-  before_action :signed_in_user, only:[:create, :destroy]
+  before_action :signed_in_user, only:[:create, :destroy, :reply]
   before_action :correct_user, only: :destroy
+  
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
@@ -16,15 +17,20 @@ class MicropostsController < ApplicationController
     @micropost.destroy
     redirect_to root_url
   end
+  
+  def reply
+    @user = User.find(params[:user])
+  end
+
+  end
 
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content)
+      params.require(:micropost).permit(:content,:in_reply_to)
     end
     
     def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
     end
-end
